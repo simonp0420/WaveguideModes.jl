4a8e127466fb73ffe4c12a8241e57747bc5b6a67
Peter Simon <psimon0420@gmail.com>
2025-10-27 16:10:14 -0700

    Update for Version 3 of PrettyTables

--- src/WaveguideModes.jl
+++ src/WaveguideModes.jl
@@ -5,13 +5,14 @@ using Unitful: Unitful, @u_str
 using MetalSurfaceImpedance: Zsurface, effective_conductivity
 using Printf: @printf
 using Accessors: @set, @reset
-using PrettyTables: pretty_table, ft_printf, tf_unicode_rounded, tf_html_default,
-                    HtmlTableFormat
+using PrettyTables: fmt__printf, HtmlTableFormat, MarkdownStyle, MarkdownTableFormat, 
+                    HtmlTableStyle, pretty_table, TextHighlighter, TextTableStyle
 using AbstractPlutoDingetjes: is_inside_pluto
 using StaticArrays: @SVector, @SMatrix
 using SimpleNonlinearSolve: SimpleNonlinearSolve as snls
 using PRIMA: bobyqa
 using FunctionZeros: besselj_zero, besselj_deriv_zero
+using Crayons: @crayon_str
 
 export @u_str
 export TE, TM, setup_modes!,
@@ -92,10 +93,83 @@ end
 """
     is_html_environment()
 
-Returns `true` if code is running under `IJulia` or `Pluto`, and `false` otherwise.
+Returns `true` if code is running under `IJulia`, `Pluto`, or `BonitoBook` and `false` otherwise.
 """
 function is_html_environment()
-    return (isdefined(Main, :IJulia) && Main.IJulia.inited) || is_inside_pluto()
+    return (isdefined(Main, :IJulia) && Main.IJulia.inited) || is_inside_pluto() || 
+    any(t -> occursin("IJulia", t) || occursin("Bonito", t) || occursin("Pluto", t), 
+        string.(typeof.(Base.Multimedia.displays)))
+end
+
+"""
+    display_mode_table(modedata::Array, titlestr::String, freq_unit, length_unit, col_fmts)
+
+Display a mode table to the user's console, or to an HTML environment.
+
+## Positional Arguments
+- `modedata`: An array of size `(nrows,6)` containing the modal data to be output.  The columns
+  contain ["Type (TE or TM)", "m", "n", "Cutoff Freq.", "Guide Wavelength", "Attenuation"]
+- `thtlestr`: A `String` containing the table title.
+- `freq_unit`: Either a `String` or an object (such as a Unitful unit) that can be converted
+  to `String` to use as a column label for the frequency column.
+- `length_unit`: Either a `String` or an object (such as a Unitful unit) that can be converted
+  to `String` to use as a column label for the Guide Wavelength column.
+- `col_fmts`: A `Vector{String}` of length 6 containing the `@sprinf` columnar formats for the
+  numeric entries in the table.
+"""
+function display_mode_table(modedata::Array, titlestr::String, freq_unit, length_unit, col_fmts)
+    size(modedata, 2) == 6 || throw(ArgumentError("Wrong number of columns in modedata"))
+    maxtitlelen = 60
+    title2 = String[]
+    nexttline = ""
+    for phrase in eachsplit(titlestr, ',')
+        if length(nexttline) + length(phrase) > maxtitlelen
+            push!(title2, nexttline)
+            nexttline = ""
+        end
+        nexttline *= string(phrase, ",")
+    end
+    push!(title2, nexttline)
+    title = first(title2)
+    subtitle = length(title2) > 1 ? join(title2[begin+1:end])[begin+1:end-1] : ""
+    column_labels = [["Type", "m", "n", "Cutoff Freq.", "Guide Wavelength", "Attenuation"]]
+    column_units = ["", "", "", "[$freq_unit]", "[$length_unit]", "[dB/$length_unit]"]
+    column_label_alignment = :c
+    push!(column_labels, column_units)
+    formatters = [fmt__printf(col_fmts[i], [i]) for i in 1:6]
+
+    if is_html_environment()
+
+        p = pretty_table(
+            modedata;
+            backend = :html,
+            style = HtmlTableStyle(; 
+                        title = ["font-weight" => "bold", "font-size" => "large"],
+                        subtitle = ["font-weight" => "bold", "font-size" => "large"],
+                        first_line_column_label = ["font-weight" => "bold"],
+                        column_label = ["font-weight" => "regular", "font-style" => "italic"],
+                            ),        
+            title,
+            subtitle,
+            column_labels,
+            formatters,
+        )
+
+    else
+
+        println()
+        p = pretty_table(
+            modedata;
+            style = TextTableStyle(; title = crayon"bold", subtitle = crayon"bold"),
+            backend = :text,
+            title,
+            subtitle,
+            column_labels,
+            formatters,
+            #highlighters = [TextHighlighter((d, i, j) -> iseven(i), crayon"negative")],
+        )
+    end
+    return nothing
 end
 
 include("RWGModes.jl")
